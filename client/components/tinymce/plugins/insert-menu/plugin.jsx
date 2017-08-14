fb09fc56c4dbc1444c2bd37e0f501d4bdb809dcd
Rastislav Lamos <lamos.rasto@gmail.com>
2017-08-14 17:50:45 +0200

    Move tooltip based on what was hovered with mouse

--- client/components/tinymce/plugins/insert-menu/plugin.jsx
+++ client/components/tinymce/plugins/insert-menu/plugin.jsx
@@ -28,6 +28,11 @@ const initialize = editor => {
 		onPostRender() {
 			ReactDOM.render( <Gridicon icon="add-outline" />, this.$el[ 0 ].children[ 0 ] );
 
+			let initialTooltipLeftPos = 0;
+			let initialTooltipTopPos = 0;
+
+			let firstTooltipRender = false;
+
 			// Listen to `mouseenter` events on the (+) part of the Inserter menu to show
 			// the "Insert content" tooltip.
 			this.$el[ 0 ].children[ 0 ].addEventListener( 'mouseenter', () => {
@@ -36,16 +41,36 @@ const initialize = editor => {
 				const btnTooltip = this.tooltip();
 
 				btnTooltip.text( i18n.translate( 'Insert content' ) );
+
+				if ( ! firstTooltipRender ) {
+					initialTooltipLeftPos = parseInt( btnTooltip.$el[ 0 ].style.left );
+					initialTooltipTopPos = parseInt( btnTooltip.$el[ 0 ].style.top );
+
+					firstTooltipRender = true;
+				}
+
+				btnTooltip.moveTo( initialTooltipLeftPos - 10, initialTooltipTopPos );
 			} );
 
 			// Listen to `mouseenter` events on the (v) part of the Inserter menu to show
 			// the "Insert special" tooltip.
-			this.$el[ 0 ].children[ 1 ].addEventListener( 'mouseenter', () => {
+			const insertSpecialEl = this.$el[ 0 ].children[ 1 ];
+
+			insertSpecialEl.addEventListener( 'mouseenter', () => {
 				// We need to select the tooltip during the `mouseenter` event and not outside.
 				// Otherwise, Tinymce renders an empty tooltip somewhere in the editor.
 				const btnTooltip = this.tooltip();
 
 				btnTooltip.text( i18n.translate( 'Insert special' ) );
+
+				if ( ! firstTooltipRender ) {
+					initialTooltipLeftPos = parseInt( btnTooltip.$el[ 0 ].style.left );
+					initialTooltipTopPos = parseInt( btnTooltip.$el[ 0 ].style.top );
+
+					firstTooltipRender = true;
+				}
+
+				btnTooltip.moveTo( initialTooltipLeftPos + 24, initialTooltipTopPos );
 			} );
 		},
 	} );
